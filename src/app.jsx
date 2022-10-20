534f9a4b7e0f9fe2c4b3d467c3f3a55f05d1cf55
bluestarburst <hargreaves.bryant@gmail.com>
2022-10-19 20:02:14 -0500

    fdgh

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,6 @@
 import { render } from "react-dom";
 import React, {useRef, useState} from "react";
 
-import First, {Second} from "./first.jsx";
-
 import Button from "@mui/material/Button";
 
 import TextField from '@mui/material/TextField';
@@ -10,47 +8,20 @@ import httpGET from "./serverAPI.js";
 
 import './styles.css';
 
-function Hello() {
-
-    httpGET("http://localhost:3000/api/first", (data) => {
-        console.log(data);
-    });
-
-    const header1 = useRef(null)
-    const text1 = useRef(null)
-
-    const [text, setText] = useState("")
-
-    
-    
-    function onTextButtonPressed() {
-        // header1.current.innerHTML = "This text changed";
-        // header1.current.innerHTML = text1.current.value;
-        // alert("You pressed the button!");
-
-        header1.current.innerHTML = text;
-    }
-    
-
-    function onTextBoxTyped(e) {
-        setText(e.target.value)
-        // header1.current.innerHTML = e.target.value;
-
-        // TextField.current.innerHTML = ""
-    }
-    return(
-        <div className="body">
+import CustomInput from "./customInput.jsx";
+import CustomOutput from "./customOutput.jsx";
 
 
-            <Button className="blue" variant="text" onClick={onTextButtonPressed}>Text</Button>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" ref={text1} onChange={onTextBoxTyped}/>
+function Page(props) {
+    <div className="body">
 
-            <h1 ref={header1} >Hello Worlds</h1>
+            <h1>Lang Portal</h1>
+            <CustomInput />
+            <CustomOutput />
         </div>
-    )
 }
 
 render(
-    <Hello />,
+    <Page />,
     document.getElementById("root")
 );
