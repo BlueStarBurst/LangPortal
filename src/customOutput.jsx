e6fd3c4e8bade6cd74219516ea5e52cb67157bc6
Joshua Mathew <joshuamathew195@gmail.com>
2022-10-30 00:38:46 -0500

    copyStyle

--- src/customOutput.jsx
+++ src/customOutput.jsx
@@ -1,17 +1,33 @@
-import React, { useRef, useState } from "react";
+import React, { useState, forwardRef } from "react";
 import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCopy } from '@fortawesome/free-solid-svg-icons'
 import Tooltip from '@mui/material/Tooltip';
 import Fade from '@mui/material/Fade';
-import Backdrop from '@mui/material/Backdrop';
+import Snackbar from '@mui/material/Snackbar';
+import Button from "@mui/material/Button";
+import MuiAlert from '@mui/material/Alert';
 
 
-
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function CustomOutput(props) {
+    const [copied,setCopied] = useState(false)
+    const [open, setOpen] = useState(false)
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+    const handleClick = () => {
+        setOpen(true);
+    };
+
     return (<div className="textF r">
         <h1>Output</h1>
 
@@ -19,6 +35,7 @@ export default function CustomOutput(props) {
 
         <CopyToClipboard text={props.translated}
             onCopy={() => setCopied(true)}
+            
         >
             <Tooltip 
                 TransitionComponent={Fade}
@@ -27,11 +44,21 @@ export default function CustomOutput(props) {
                 placement="top-start" 
                 arrow
             >
-                <IconButton>
+                <Button variant="contained" onClick={handleClick}>
                     <FontAwesomeIcon icon={faCopy}/>
-                </IconButton>
+                </Button>
             </Tooltip>
         </CopyToClipboard>
+        <Snackbar
+        autoHideDuration={4000}
+        open={open}
+        onClose={handleClose}
+        >
+            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                Copied!
+            </Alert>
+        </Snackbar>
+        
     </div>
 
     )
