ba666d30d842a9fbe3080727a67bb7f29e9ed5b3
Joshua Mathew <joshuamathew195@gmail.com>
2022-11-29 12:15:17 -0600

    Josh

--- src/customOutput.jsx
+++ src/customOutput.jsx
@@ -29,7 +29,7 @@ export default function CustomOutput(props) {
     };
 
     return (<div className="textF r">
-        <h1>Output</h1>
+        <h1>Spanish</h1>
 
         <TextField multiline minRows={8} className="w-100 h-100 tex" id="outlined-basic" label="Output Text" variant="outlined" value={props.translated} disabled onChange={({ target: { value } }) => setValue(value)} />
         <div className="copy2">
