2dc69d1f4a2cb520d225f11d803326b52198740b
Joshua Mathew <joshuamathew195@gmail.com>
2022-12-01 23:57:21 -0600

    dark mode

--- src/customInput.jsx
+++ src/customInput.jsx
@@ -18,7 +18,7 @@ export default function CustomInput(props) {
     return (
         <div className="textF l">
             <h1 className="lang">English</h1>
-            <TextField inputRef={inputText} multiline minRows={8} className="w-100 h-100 tex" id="outlined-basic" label="Input Text" variant="outlined" onChange={props.charTyped} onKeyDown={props.keyDown} />
+            <TextField inputRef={inputText} multiline minRows={8} className="w-100 h-100 tex" id="outlined-basic" label="Input Text" variant="outlined" onChange={props.charTyped} onKeyDown={props.keyDown} InputProps={{className:"textarea"}} />
             <div className="copy2">
                 <SpeechToText setResponse={stt} />
             </div>
